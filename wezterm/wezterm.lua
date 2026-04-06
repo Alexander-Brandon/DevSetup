eeaeed49f232420f401a5e1dc607f20d4ea7a903
brandon <brandon@brandonalexander.io>
2026-04-05 19:53:00 -0500

    remove my popterm implementation

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -19,38 +19,6 @@ config.color_scheme = 'Tokyo Night'
 config.enable_kitty_keyboard = true
 config.custom_block_glyphs = false
 
-local popterm_panes = {} -- tab_id -> pane_id
-
-local function pop_term(window, pane)
-  local tab = window:active_tab()
-  local tab_id = tab:tab_id()
-
-  -- If we have a tracked PopTerm, try to close it
-  local existing_id = popterm_panes[tab_id]
-  if existing_id then
-    for _, p in ipairs(tab:panes()) do
-      if p:pane_id() == existing_id then
-        p:activate()
-        window:perform_action(wezterm.action.CloseCurrentPane { confirm = false }, p)
-        popterm_panes[tab_id] = nil
-        return
-      end
-    end
-    -- Pane no longer exists, clear stale entry
-    popterm_panes[tab_id] = nil
-  end
-
-  -- Open PopTerm in the cwd of the current pane
-  local cwd_uri = pane:get_current_working_dir()
-  local cwd = cwd_uri and cwd_uri.file_path or wezterm.home_dir
-
-  local new_pane = pane:split {
-    direction = "Bottom",
-    size = 0.35,
-    cwd = cwd,
-  }
-  popterm_panes[tab_id] = new_pane:pane_id()
-end
 
 config.keys = {
   {
@@ -78,12 +46,7 @@ config.keys = {
     mods = "ALT",
     action = wezterm.action.CloseCurrentPane { confirm = false },
   },
-  {
-    key = "h",
-    mods = "ALT",
-    action = wezterm.action_callback(pop_term),
-  },
-  { key = "[", mods = "ALT", action = wezterm.action.MoveTabRelative(-1) },
+{ key = "[", mods = "ALT", action = wezterm.action.MoveTabRelative(-1) },
   { key = "]", mods = "ALT", action = wezterm.action.MoveTabRelative(1) },
 }
 
