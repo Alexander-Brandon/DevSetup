753b1a4494945eb092a2b0a9943ad2ac851e5937
brandon <brandon@brandonalexander.io>
2026-04-05 19:37:40 -0500

    updates for options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,17 @@
 vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
+
+-- Scroll
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
+vim.opt.cursorline = true
+
+-- Undo
+vim.opt.undofile = true
+vim.opt.swapfile = false
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
