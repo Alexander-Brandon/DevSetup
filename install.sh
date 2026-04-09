d42dbc97cfb9ceb62b468e45870737d65fd21bff
Brandon Alexander <brandon@brandonalexander.io>
2026-04-08 23:59:08 -0500

    adding a tmux conf

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ NVIM_SRC="$SCRIPT_DIR/nvim"
 NVIM_DEST="$HOME/.config/nvim"
 WEZTERM_SRC="$SCRIPT_DIR/wezterm/wezterm.lua"
 WEZTERM_DEST="$HOME/.config/wezterm/wezterm.lua"
+TMUX_SRC="$SCRIPT_DIR/tmux/tmux.conf"
+TMUX_DEST="$HOME/.config/tmux/tmux.conf"
 
 # Ensure ~/.config exists
 mkdir -p "$HOME/.config"
@@ -33,3 +35,15 @@ fi
 ln -s "$WEZTERM_SRC" "$WEZTERM_DEST"
 
 echo "Done: $WEZTERM_DEST -> $WEZTERM_SRC"
+
+# Tmux config
+mkdir -p "$HOME/.config/tmux"
+if [ -L "$TMUX_DEST" ]; then
+  rm "$TMUX_DEST"
+elif [ -f "$TMUX_DEST" ]; then
+  rm "$TMUX_DEST"
+fi
+
+ln -s "$TMUX_SRC" "$TMUX_DEST"
+
+echo "Done: $TMUX_DEST -> $TMUX_SRC"
