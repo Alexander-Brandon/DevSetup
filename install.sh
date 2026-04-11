7c2b7b810e65b493b3efde55a075190f9de96a34
Brandon Alexander <brandon@brandonalexander.io>
2026-04-11 00:20:51 -0500

    removed wezterm

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 NVIM_SRC="$SCRIPT_DIR/nvim"
 NVIM_DEST="$HOME/.config/nvim"
-WEZTERM_SRC="$SCRIPT_DIR/wezterm/wezterm.lua"
-WEZTERM_DEST="$HOME/.config/wezterm/wezterm.lua"
 TMUX_SRC="$SCRIPT_DIR/tmux/tmux.conf"
 TMUX_DEST="$HOME/.config/tmux/tmux.conf"
 
@@ -23,18 +21,6 @@ fi
 ln -s "$NVIM_SRC" "$NVIM_DEST"
 
 echo "Done: $NVIM_DEST -> $NVIM_SRC"
-
-# Wezterm config
-mkdir -p "$HOME/.config/wezterm"
-if [ -L "$WEZTERM_DEST" ]; then
-  rm "$WEZTERM_DEST"
-elif [ -f "$WEZTERM_DEST" ]; then
-  rm "$WEZTERM_DEST"
-fi
-
-ln -s "$WEZTERM_SRC" "$WEZTERM_DEST"
-
-echo "Done: $WEZTERM_DEST -> $WEZTERM_SRC"
 
 # Tmux config
 mkdir -p "$HOME/.config/tmux"
