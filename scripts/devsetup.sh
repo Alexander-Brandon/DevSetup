9da8ea00c190d931e54e14763b8d27b0ff4e6d06
Brandon Alexander <brandon@brandonalexander.io>
2026-04-19 01:50:39 -0500

    Tmux setup scripts

--- scripts/devsetup.sh
+++ scripts/devsetup.sh
@@ -2,31 +2,34 @@
 
 WORKSPACE=${1:-Workspace}
 PROJECT_DIR=${2:-~/Documents/projects}
+
+# GUARD for tmux check
+command -v tmux &>/dev/null || { echo "INSTALL TMUX BEFORE RUNNING THE SCRIPT"; exit 1; }
+
 # GUARD for already setup 
-if tmux has-session -t $WORKSPACE 2>/dev/null;
+if tmux has-session -t $WORKSPACE 2>/dev/null; then
   echo "$WORKSPACE already exists"
   tmux attach -t $WORKSPACE
   exit 0
 fi
 
 # GUARD for no project
-if [-d "$PROJECT_DIR"] || { echo "$PROJECT_DIR not found"; exit 1; }
+[ -d "$PROJECT_DIR" ] || { echo "$PROJECT_DIR not found"; exit 1; }
 
 # Neovim Section
 tmux new-session -d -s $WORKSPACE -x $(tput cols) -y $(tput lines)
-tmux send-keys -t $WORKSPACE cd "PROJECT_DIR && nvim"
+tmux send-keys -t $WORKSPACE cd "$PROJECT_DIR && nvim"
 
 # AI Section
 tmux split-window -h -t $WORKSPACE -p 25
 tmux send-keys -t $WORKSPACE "claude" Enter
 
-# Neovim Section
-tmux new-session -d -s $WORKSPACE -x $(tput cols) -y $(tput lines)
-tmux send-keys -t $WORKSPACE cd "PROJECT_DIR" 
+# Console Session
+tmux new-session -d -s ${WORKSPACE}_Terminal -x $(tput cols) -y $(tput lines)
+tmux send-keys -t ${WORKSPACE}_Terminal cd "$PROJECT_DIR" 
 
-# AI Section
-tmux split-window -h -t $WORKSPACE -p 25
-tmux send-keys -t $WORKSPACE "claude" Enter
+# Secondary Console Section
+tmux split-window -h -t ${WORKSPACE}_Terminal -p 25
 
 tmux attach -t $WORKSPACE
 
